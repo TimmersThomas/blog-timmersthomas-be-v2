51c2cc417525ceeb73edf4cfcf69c03988019a17
Thomas Timmers <me@timmersthomas.be>
2022-01-26 20:59:01 +0100

    Fixes heading + prefetch first post

--- components/structure/intro.tsx
+++ components/structure/intro.tsx
@@ -1,16 +1,26 @@
-import TextLink from "../generic/textLink"
+import TextLink from "../generic/textLink";
 
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-center md:text-left text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
-        Just a simple <span className="text-3xl italic">&apos;tech&apos;</span> blog
+    <section>
+      <h1 className="flex-col md:flex-row flex-wrap	 flex items-baseline content-end md:justify-between mt-16 mb-16 md:mb-12">
+        <span className="text-center md:text-left text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8 w-full md:w-auto">
+          Just a simple{" "}
+          <span className="text-3xl italic">&apos;tech&apos;</span> blog
+        </span>
+        <span className="text-center md:text-right text-lg md:pl-8 w-full md:w-auto">
+          by{" "}
+          <TextLink
+            href="https://timmersthomas.be"
+            target="_blank"
+            rel="noopener"
+          >
+            Timmers Thomas
+          </TextLink>
+        </span>
       </h1>
-      <span className="text-center md:text-left text-lg mt-5 md:pl-8">
-        by <TextLink href="https://timmersthomas.be" target="_blank" rel="noopener" >Timmers Thomas</TextLink>
-      </span>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
